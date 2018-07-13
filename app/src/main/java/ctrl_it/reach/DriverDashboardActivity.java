057a5510f4e3dbf2736bff926a49073a1b0accb8
jordonocampbell <jordonocampbell@gmail.com>
2018-07-13 11:43:21 -0500

    Committed changes to Jourdan's master in his fork?
    Signed-off-by: jordonocampbell <jordonocampbell@gmail.com>
    
    # Conflicts:
    #	app/src/main/java/ctrl_it/reach/DriverDashboardActivity.java
    #	app/src/main/res/layout/activity_driver_dashboard.xml
    #	app/src/main/res/layout/fragment_driver_ride_schedule.xml

--- app/src/main/java/ctrl_it/reach/DriverDashboardActivity.java
+++ app/src/main/java/ctrl_it/reach/DriverDashboardActivity.java
@@ -5,56 +5,70 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-// TODO: 12/7/2018 implement new de sign inclusive of: hamburger button for navigation bar use and reduce ride schedule to one button
+import ctrl_it.reach.Fragments.DriverScheduleFragment;
+
+
 public class DriverDashboardActivity extends AppCompatActivity{
     private DrawerLayout mDrawerLayout;
-    private NavigationView driverNavigationView;
-    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_dashboard);
 
-        driverNavigationView = findViewById(R.id.nav_view);
-        setupDrawerContent(driverNavigationView);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        setupDrawerContent(navigationView);
+
     }
 
-    private void setupDrawerContent(NavigationView driverNavigationView){
-        driverNavigationView.setNavigationItemSelectedListener(
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(MenuItem menuItem) {
-                    mDrawerLayout = findViewById(R.id.drawer_layout);
-                    selectDrawerItem(menuItem);
-                    return true;
-                }
-            }
-        );
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
     }
 
-    public void selectDrawerItem(MenuItem menuItem){
-        // Add code here to update the UI based on the item selected
-        // For example, swap UI fragments here
-
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
         Fragment fragment = null;
         Class fragmentClass;
         switch(menuItem.getItemId()) {
-            case R.id.fragment_driver_account_details:
-                fragmentClass = DriverAccountDetails.class;
-                break;
             case R.id.fragment_driver_ride_schedule:
-                fragmentClass = DriverRideSchedule.class;
+                fragmentClass = DriverScheduleFragment.class;
+                Toast.makeText(this, "fragment 1",
+                        Toast.LENGTH_LONG).show();
                 break;
             default:
-                fragmentClass = DriverAccountDetails.class;
+                fragmentClass = DriverScheduleFragment.class;
         }
 
         try {
@@ -67,6 +81,7 @@ public class DriverDashboardActivity extends AppCompatActivity{
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
+        // Highlight the selected item has been done by NavigationView
         menuItem.setChecked(true);
         // Set action bar title
         setTitle(menuItem.getTitle());
